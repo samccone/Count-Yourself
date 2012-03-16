ff8e21c659b7e43c07e68675ed8c2593b11a2f5f
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2012-03-16 17:00:27 -0400

    add OS X logger files

--- README.markdown
+++ README.markdown
@@ -4,13 +4,22 @@ Overview
 Finds a keyboard device and monitors for ACCESS events
 When access events are fired, the user's MAC address with a keyboard ACCESS count is piped through `netcat`
 
-CLIENT:
+CLIENT For Linux:
 -----------
 
 Will need su access (or `sudo`) to monitor the `/dev/input/event*`
 Additionally, the `inotify-tools` package needs to be installed
 
     sudo apt-get install inotify-tools
+
+CLIENT For OS X:
+-----------
+
+	sudo osx_logger_bin
+	
+If you need to recompile from the `osx_logger.c` file, compile with the following command
+
+    gcc -Wall -o osx_logger osx_logger.c -framework ApplicationServices
 
 SERVER:
 -----------
@@ -21,7 +30,11 @@ It will receive an event from a client at most every 5 seconds. If the client do
 
     <MAC address>|<keypress count>
 
-* TODO Get this working on Apple OS X (with probably use kqueue/kevent)
+* TODO Get this working on Apple OS X 
+	* with probably use kqueue/kevent - 
+	* https://github.com/gorakhargosh/watchdog
+	* http://code.google.com/p/logkext/
+	* http://danielbeard.wordpress.com/2010/10/29/listening-for-global-keypresses-in-osx/
 * TODO Decode key pressed, and send character combination instead
 * TODO Weigh keys distance from the home row and send value along with character
 * TODO Record mouse clicks
---
+++ osx/osx_logger.c
@@ -0,0 +1,52 @@
+#include <ApplicationServices/ApplicationServices.h>
+CGEventRef
+myCGEventCallback(CGEventTapProxy proxy, CGEventType type,
+                  CGEventRef event, void *refcon)
+{
+    // Paranoid sanity check.
+    if ((type != kCGEventKeyDown) && (type != kCGEventKeyUp))
+        return event;
+
+    // The incoming keycode.
+    CGKeyCode keycode = (CGKeyCode)CGEventGetIntegerValueField(
+                                       event, kCGKeyboardEventKeycode);
+
+    //Keypress code goes here.
+    fprintf(stdout,"press\n");
+
+    // We must return the event for it to be useful.
+    return event;
+}
+
+int
+main(void)
+{
+    CFMachPortRef      eventTap;
+    CGEventMask        eventMask;
+    CFRunLoopSourceRef runLoopSource;
+
+    // Create an event tap. We are interested in key presses.
+    eventMask = ((1 << kCGEventKeyUp));
+    eventTap = CGEventTapCreate(kCGSessionEventTap, kCGHeadInsertEventTap, 0,
+                                eventMask, myCGEventCallback, NULL);
+    if (!eventTap) {
+        fprintf(stderr, "failed to create event tap\n");
+        exit(1);
+    }   
+
+    // Create a run loop source.
+    runLoopSource = CFMachPortCreateRunLoopSource(
+                        kCFAllocatorDefault, eventTap, 0);
+
+    // Add to the current run loop.
+    CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource,
+                       kCFRunLoopCommonModes);
+
+    // Enable the event tap.
+    CGEventTapEnable(eventTap, true);
+
+    // Set it all running.
+    CFRunLoopRun();
+
+    exit(0);
+}
