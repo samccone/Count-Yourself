c0308ce377b5bfc9da6bbb92dce008cc9e20f134
Sam Saccone <samccone@gmail.com>
2012-03-15 20:34:37 -0400

    handle case if the server bombs

--- fake_client.js
+++ fake_client.js
@@ -1,13 +1,22 @@
 var net		= require('net');
 var id		= (Math.floor(Math.random()*1048577)).toString(16);
-var c	= net.connect(12345,function(){
+var connected = false;
+var c	= net.connect(12345);
+
+c.on('close',function(){
+	console.log('disconnect');
+	connected=false;
+});
+
+c.on('connect',function(){
+	console.log('connect');
 	sendMessage();
-	console.log("connected");
+	connected=true;
 });
 
 
 function sendMessage(){
 	var count = Math.floor(Math.random() * 301);
-	c.write(id+"|"+count);
+	connected && c.write(id+"|"+count);
 	setTimeout(sendMessage,5000);
 }
