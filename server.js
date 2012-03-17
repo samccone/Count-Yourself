45fc43369911af4f212e2ad1e8a1d7a4ea515f21
Sam Saccone <samccone@gmail.com>
2012-03-17 18:52:40 -0400

    send real time updates to client with socket.io

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var tcpServer = require('./tcpServer/server.js');
 
 var webServer = require('./webServer/server.js').start({
 	'port': 1337,
-	'data': tcpServer.getData
+	'data': tcpServer.getData,
+	'newData' : tcpServer.onNewData
 });
 
 tcpServer.start();
--- webServer/server.js
+++ webServer/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var webServer = express.createServer();
+var io			= require('socket.io').listen(webServer);
 var data;
+var connections = {};
 
 webServer.configure(function(){
 	webServer.set("view engine","jade");
@@ -12,10 +14,27 @@ webServer.get( '/', function( req, res ){
 	res.render('home');
 });
 
+io.on('connection',addConnection);
+
+
+function addConnection(s){
+	connections[s.id] = s;
+	s.on('disconnect',function(s){
+		delete connections[s.id];
+	});
+}
+
+function sendDataUpdates(data){
+	console.log("new data");
+	for(c in connections){
+		connections[c].emit('message',data);
+	}
+}
 function startServer(args){
 		data = args.data;
 		console.log("web server started on port: "+ args.port);
 		webServer.listen(args.port);
+		args.newData(sendDataUpdates);
 }
 
 
