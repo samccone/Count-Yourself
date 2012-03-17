7049647366d0c2d7be82e77374bc9aee1325066d
Sam Saccone <samccone@gmail.com>
2012-03-17 17:05:14 -0400

    add jade for markup rendering

--- webServer/server.js
+++ webServer/server.js
@@ -2,6 +2,12 @@ var express = require('express');
 var webServer = express.createServer();
 var data;
 
+webServer.configure(function(){
+	webServer.set("view engine","jade");
+	webServer.set('views', __dirname + '/views');
+	webServer.set('views');
+});
+
 webServer.get( '/', function( req, res ){
 	res.send(JSON.stringify(data()));
 });
