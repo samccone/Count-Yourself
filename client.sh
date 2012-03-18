2609715258549b6ea3462e4b620b7d3d732c090a
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2012-03-18 12:03:03 -0400

    dont let client.sh bomb on os x

--- client.sh
+++ client.sh
@@ -7,13 +7,17 @@ if [ "$OS" == "Linux" ]; then
   MAC=`ifconfig | grep HWaddr | head -1 | awk '{print $5}'`
   EVENT=`grep -E 'Handlers|EV=' /proc/bus/input/devices | grep -B1 'EV=120013' | grep -Eo 'event[0-9]+' | tail -1`
   KBD=/dev/input/$EVENT
+
+	while RES=$(sudo inotifywatch -z -e access -t 5 $KBD); do
+	  PRESSES=`echo $RES | tail -1 | awk '{print $4}'`
+	  if [ -n "$PRESSES" ]; then
+	    echo "${MAC}|${PRESSES}" | nc $SERVER
+	  fi
+	done
+
 elif [ "$OS" == "Darwin" ]; then
   MAC=`ifconfig | grep 'ether' | head -1 | awk '{print $2}'`
+
+	echo "OS X Client Not Available Yet"
 fi
 
-while RES=$(sudo inotifywatch -z -e access -t 5 $KBD); do
-  PRESSES=`echo $RES | tail -1 | awk '{print $4}'`
-  if [ -n "$PRESSES" ]; then
-    echo "${MAC}|${PRESSES}" | nc $SERVER
-  fi
-done
