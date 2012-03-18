c684d5e4e40b2658dc42f231adf3b57f103e1594
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2012-03-18 13:35:44 -0400

    updates to C code to include mac addy and port

--- osx/osx_logger.c
+++ osx/osx_logger.c
@@ -1,8 +1,17 @@
 #include <ApplicationServices/ApplicationServices.h>
-CGEventRef
-myCGEventCallback(CGEventTapProxy proxy, CGEventType type,
-                  CGEventRef event, void *refcon)
-{
+
+struct Config {
+	char *mac;
+	char *port;
+	// int port;
+};
+
+CGEventRef myCGEventCallback(CGEventTapProxy proxy, CGEventType type,
+                  					 CGEventRef event, void *refcon) {
+	
+		// get our config
+		struct Config *cfg = refcon;
+
     // Paranoid sanity check.
     if ((type != kCGEventKeyDown) && (type != kCGEventKeyUp))
         return event;
@@ -12,23 +21,34 @@ myCGEventCallback(CGEventTapProxy proxy, CGEventType type,
                                        event, kCGKeyboardEventKeycode);
 
     //Keypress code goes here.
-    fprintf(stdout,"press\n");
+    fprintf(stdout,"press keycode: %d\n", keycode);
 
     // We must return the event for it to be useful.
     return event;
 }
 
-int
-main(void)
-{
+int main(int argc, char *argv[]) {
     CFMachPortRef      eventTap;
     CGEventMask        eventMask;
     CFRunLoopSourceRef runLoopSource;
 
+
+		// print the MAC address input param if we have it
+		if (argc < 2) {
+			printf("You must supply the MAC address and Port as a param: \"./osx_logger 12:34:56:78 1337\n\"");
+			exit(1);
+		}
+
+		// store our config settings
+		struct Config c, *cfg = &c;
+		c.mac = argv[1];
+		c.port = argv[2];
+		printf("mac: %s\tport: %s\n\n", cfg->mac, cfg->port);
+
     // Create an event tap. We are interested in key presses.
     eventMask = ((1 << kCGEventKeyUp));
     eventTap = CGEventTapCreate(kCGSessionEventTap, kCGHeadInsertEventTap, 0,
-                                eventMask, myCGEventCallback, NULL);
+                                eventMask, myCGEventCallback, cfg);
     if (!eventTap) {
         fprintf(stderr, "failed to create event tap\n");
         exit(1);
